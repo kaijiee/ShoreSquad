60343ff467b9405e59562a307456f1ac7d464ed5
kaijiee <23041646@myrp.edu.sg>
2025-06-05 09:57:52 +0800

    Add config template and update gitignore to protect API key

---
+++ js/config.template.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    // Get your API key from https://openweathermap.org/api
+    WEATHER_API_KEY: 'your_api_key_here',
+    // Singapore center coordinates
+    MAP_CENTER: [1.251401, 103.817045],
+    // Beach locations
+    BEACHES: {
+        siloso: {
+            name: 'Siloso Beach',
+            coordinates: [1.254577, 103.811363],
+            description: 'Popular beach on Sentosa Island'
+        },
+        palawan: {
+            name: 'Palawan Beach',
+            coordinates: [1.248225, 103.822727],
+            description: 'Family-friendly beach with suspension bridge'
+        }
+    }
+};
